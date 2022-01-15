b0f1f2b59d76580e81d31f19008481a305769268
suryawanshivp <suryawanshivp@gmail.com>
2022-01-15 15:52:23 +0530

    change in comment

--- src/main/java/com/springboot/first/app/HelloWorldController.java
+++ src/main/java/com/springboot/first/app/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 	//http://localhost:8090/hello-world
 	@GetMapping("/hello-world") 
 	public String helloworld() {
-		return "Hello Simplilearn...Welcome to Bookzy(online book shop)!";
+		return "Hello Simplilearn...Welcome to Bookzy...welcome again!";
 	}
 
 }
